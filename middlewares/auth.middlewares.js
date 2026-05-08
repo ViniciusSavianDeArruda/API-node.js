fa3a17ce2eb542454552fd325b45aae8da8287ee
Vinicius Arruda <arruda.vinicius@ufn.edu.br>
2026-05-08 10:56:48 -0300

    feat: adiciona middleware de validação JWT

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) => {
+
+  // pegar authorization header
+  const authHeader = req.headers.authorization;
+
+  // verificar se token existe
+  if (!authHeader) {
+
+    return res.status(401).json({
+      message: "Token não informado"
+    });
+  }
+
+  // pegar somente token
+  const token = authHeader.split(" ")[1];
+
+  try {
+
+    // validar token
+    jwt.verify(token, "secret-key");
+
+    // continuar rota
+    next();
+
+  } catch (error) {
+
+    return res.status(401).json({
+      message: "Token inválido"
+    });
+  }
+};
