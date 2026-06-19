f3af9568a940196180a6322de4e4bf298d58399f
Vinicius Arruda <arrudavinicius283@gmail.com>
2026-06-19 11:29:28 -0300

    refactor: centraliza array de usuários em users.js e corrige imports dos controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,33 +1,27 @@
 import jwt from "jsonwebtoken";
+import { users } from "../users.js";
 
 // função de login
 export const login = (req, res) => {
-
   const { email, password } = req.body;
 
-  // Verificar email e senha
-  if (email !== "admin@gmail.com" || password !== "123456") {
+    const user = users.find(u => u.email === email);
 
-    return res.status(401).json({
-      message: "Email ou senha inválidos"
-    });
+  // Verificar email e senha
+   if (!user || user.password !== password) {
+    return res.status(401).json({ message: "Email ou senha inválidos" });
   }
 
   // gerar token
   const token = jwt.sign(
     {
-      email
-    },
-
-    "secret-key",
-
-    {
-      expiresIn: "1d"
-    }
+      id: user.id,
+      email: user.email
+  },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
   );
 
   // devolver token
-  return res.json({
-    token
-  });
+  return res.json({token});
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,4 @@
-import jwt from "jsonwebtoken"; // importando jsonwebtoken para criar tokens de autenticação
-// criando um array para armazenar os usuários sem banco de dados
-let users = [];
+import { users } from "../users.js";
 
 export const getUsers = (req, res)=>{ // => isso é um callback, funcao que passa como parametro para outra funcao
   return res.json(users);
@@ -12,28 +10,35 @@ export const getUsers = (req, res)=>{ // => isso é um callback, funcao que pass
 
 //criar usuario
 export const createUser = (req, res)=>{
-
-  const {name, email} = req.body; //cliente manda nome e email para o servidor
+  const {name, email, password} = req.body; //cliente manda nome e email para o servidor
 
   // criando objeto do usuário
   const user = {
     id: users.length + 1,
     name,
-    email
+    email,
+    password
   };
 
   users.push(user); // adicionando o usuario no array de usuarios
 
   return res.status(201).json(user); // status 201 - criado, para indicar que o usuario foi criado com sucesso
-} 
+}
 
 
 //Deletar usuario
 export const deleteUser = (req, res)=>{
+  const { id } = req.params; //cliente manda o id do usuario para o servidor
 
-  const {id} = req.params; //cliente manda o id do usuario para o servidor
+  // Encontrar o índice do usuário no array
+  const index = users.findIndex(user => user.id == id);
 
-  users = users.filter(user => user.id != id); // removendo o usuario do array de usuarios
+  // Verificar se o usuário existe
+  if (index === -1) {
+    return res.status(404).json({ message: "Usuário não encontrado" });
+  }
+
+  users.splice(index, 1);
 
   return res.sendStatus(204); // status 204 - no content, para indicar que o usuario foi deletado com sucesso
 }
