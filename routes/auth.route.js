b41dc887236d14c5c4fe92b6a9860583a53c918e
Vinicius Arruda <arrudavinicius283@gmail.com>
2026-07-01 22:47:18 -0300

    feat: adiciona refresh token com persistência no banco

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
-
-import { login } from "../controllers/authController.js";
+import { login, refreshToken } from "../controllers/authController.js";
 
 const router = Router();
 
@@ -38,7 +37,32 @@ const router = Router();
  *       401:
  *         description: Email ou senha inválidos
  */
-
 router.post("/login", login);
+
+/**
+ * @openapi
+ * /auth/refresh-token:
+ *   post:
+ *     summary: Gera um novo access token a partir do refresh token
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - refreshToken
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Novo access token gerado com sucesso
+ *       401:
+ *         description: Refresh token inválido ou expirado
+ */
+router.post("/refresh-token", refreshToken);
 
 export default router;
